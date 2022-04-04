dff2f3d8512ba0b7a91dc3a6c6dadc5c8e1723e0
gracetessy <graceabrahamimaji@gmail.com>
2022-04-03 18:05:20 -0700

    all

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -3,8 +3,8 @@
 /**
  * _strcat - concatenates  the string pointed to by @src to
  * the end of the string pointed to by @dest
- * @dest: String that will be appended
- * @src: String to be concatenated upon
+ *@dest: String that will be appended
+ *@src: String to be concatenated upon
  *
  * Return: returns poiner to @dest
  */
