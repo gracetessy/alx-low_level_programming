d96dd85dfbb9bfc3d0258613d2028147c6e53a69
gracetessy <graceabrahamimaji@gmail.com>
2022-04-14 19:32:46 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -7,7 +7,7 @@
  *
  * Return: no return.
  */
-void print_numbers(const char *separator, const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list valist;
 	unsigned int i;
