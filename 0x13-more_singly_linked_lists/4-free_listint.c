2bb819c4ac569c7b2f929188a73a58fd905b5d25
gracetessy <graceabrahamimaji@gmail.com>
2022-04-26 08:59:34 -0700

    4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -11,8 +11,8 @@ void free_listint(listint_t *head)
 	listint_t *temp;
 
 	while ((temp = head) != NULL)
-		{
-			head = head->next;
-			free(temp);
-		}
+	{
+		head = head->next;
+		head = head->next;
+	}
 }
